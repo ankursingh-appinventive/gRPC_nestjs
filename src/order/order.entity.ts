db376f0cd189af411da74d0b069c6f844fb36f0b
ankursingh-appinventive <ankur.singh1@appinventiv.com>
2023-10-30 15:14:41 +0530

    order service
    this branch consists of order microservice feature

---
+++ src/order/order.entity.ts
@@ -0,0 +1,22 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Order extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  public id!: number;
+
+  @Column({ type: 'decimal', precision: 12, scale: 2 })
+  public price!: number;
+
+  /*
+   * Relation IDs
+   */
+//   @Column({ type: 'integer'})
+//   public orderId!: number;
+
+  @Column({ type: 'integer' })
+  public productId!: number;
+
+  @Column({ type: 'integer' })
+  public userId!: number;
+}
